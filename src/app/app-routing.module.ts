72878b23a6ad936277ac564b1927f5360b9ab7a1
Adithya Thiruvalluvan <adithya.thiruvalluvan@gmail.com>
2020-07-15 11:54:03 -0400

    setup basic routing with html templates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from'./home/home.component';
+import { AssetsComponent } from'./assets/assets.component';
+import { DetailsComponent } from'./details/details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home-component', component: HomeComponent },
+  { path: 'assets-component', component: AssetsComponent },
+  { path: 'details-component', component: DetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
